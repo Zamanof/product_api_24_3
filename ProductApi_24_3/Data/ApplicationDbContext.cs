c6448364f23d02a918d9b09bdddabd03ed5fd7b1
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-08-17 20:13:37 +0400

    productsite

---
+++ ProductApi_24_3/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using MyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) 
+        : base(options)
+    {}
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>(
+            entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Price).HasColumnType("decimal(10, 2)");
+            });
+
+        modelBuilder.Entity<Product>().HasIndex(p => new { p.IsDiscountActive, p.DiscountStart, p.DiscountEnd })
+            .HasDatabaseName("Prosucts_IsDiscountActive_DiscountStart_DiscountEnd");
+    }
+
+}
